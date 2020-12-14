a5def3e1745cd3522a039c2374adc1724da0267c
Toomas Tahves <toomastahves@hotmail.com>
2020-12-14 19:27:29 +0200

    feat: update broadcast format

--- serial.js
+++ serial.js
@@ -1,5 +1,6 @@
 // todo: move serial code from server here
 const SerialPort = require('serialport');
+const fs = require('fs');
 
 const port = new SerialPort('COM3', {
     baudRate: 115200
@@ -15,45 +16,50 @@ function handleError(error) {
 }
 
 let counter = 0;
-let arr = [];
+let counts = Array(4095).fill(0);
+
 function handleData(buffer) {
-    console.log(buffer);
     let numbers = JSON.parse(JSON.stringify(buffer)).data;
 
     if(numbers[0] === 77 && numbers[1] === 67 && numbers[2] === 65) { // Check if first three codes are MCA
         const result = numbers[3] * 256 + numbers[4]; // Calculate result in decimal
-        if(result > 20 && result < 65533) { // Filter outliers
-            arr.push(result);
+        if(result > 40 && result < 4096) { // Filter outliers
+            counts[result]++;
+            counter++;
         }
     }
     if(numbers[1] === 77 && numbers[2] === 67 && numbers[3] === 65) { // Check if first three codes are MCA
         const result = numbers[4] * 256 + numbers[0]; // Calculate result in decimal
-        if(result > 20 && result < 65533) { // Filter outliers
-            arr.push(result);
+        if(result > 40 && result < 4096) { // Filter outliers
+            counts[result]++;
+            counter++;
         }
     }
     if(numbers[2] === 77 && numbers[3] === 67 && numbers[4] === 65) { // Check if first three codes are MCA
         const result = numbers[0] * 256 + numbers[1]; // Calculate result in decimal
-        if(result > 20 && result < 65533) { // Filter outliers
-            arr.push(result);
+        if(result > 40 && result < 4096) { // Filter outliers
+            counts[result]++;
+            counter++;
         }
     }
     if(numbers[3] === 77 && numbers[4] === 67 && numbers[0] === 65) { // Check if first three codes are MCA
         const result = numbers[1] * 256 + numbers[2]; // Calculate result in decimal
-        if(result > 20 && result < 65533) { // Filter outliers
-            arr.push(result);
+        if(result > 40 && result < 4096) { // Filter outliers
+            counts[result]++;
+            counter++;
         }
     }
     if(numbers[4] === 77 && numbers[0] === 67 && numbers[1] === 65) { // Check if first three codes are MCA
         const result = numbers[2] * 256 + numbers[3]; // Calculate result in decimal
-        if(result > 20 && result < 65533) { // Filter outliers
-            arr.push(result);
+        if(result > 40 && result < 4096) { // Filter outliers
+            counts[result]++;
+            counter++;
         }
     }
 
-    counter++;
-    if(counter === 10) {
-        console.log(arr);
-        process.exit(1);
+    if(counter % 1000 === 0) {
+        var data = counts.join(",");
+        fs.writeFileSync('data.csv', data);
+        console.log('Saved to file ', counter);
     }
 }
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const options = {
 let server = http.createServer((req, res) => {
     res.writeHead(200);
 });
-server.listen(2087, () => console.log('Http running.'));
+server.listen(5002, () => console.log('Http running.'));
 
 const wss = new ws.Server({server, path: '/gamma'});
 
@@ -72,49 +72,52 @@ function handleError(error) {
     console.log(error);
 }
 
-counter = 0;
+let counter = 0;
+let counts = Array(4095).fill(0);
 
 function handleData(buffer) {
     let numbers = JSON.parse(JSON.stringify(buffer)).data;
-    let validatedResult = 0;
 
     if(numbers[0] === 77 && numbers[1] === 67 && numbers[2] === 65) { // Check if first three codes are MCA
-        result = numbers[3] * 256 + numbers[4]; // Calculate result in decimal
-        if(result > 20 && result < 4100) { // Filter outliers
-            validatedResult = result;
+        const result = numbers[3] * 256 + numbers[4]; // Calculate result in decimal
+        if(result > 40 && result < 4096) { // Filter outliers
+            counts[result]++;
+            counter++;
         }
     }
     if(numbers[1] === 77 && numbers[2] === 67 && numbers[3] === 65) { // Check if first three codes are MCA
-        result = numbers[4] * 256 + numbers[0]; // Calculate result in decimal
-        if(result > 20 && result < 4100) { // Filter outliers
-            validatedResult = result;
+        const result = numbers[4] * 256 + numbers[0]; // Calculate result in decimal
+        if(result > 40 && result < 4096) { // Filter outliers
+            counts[result]++;
+            counter++;
         }
     }
     if(numbers[2] === 77 && numbers[3] === 67 && numbers[4] === 65) { // Check if first three codes are MCA
-        result = numbers[0] * 256 + numbers[1]; // Calculate result in decimal
-        if(result > 20 && result < 4100) { // Filter outliers
-            validatedResult = result;
+        const result = numbers[0] * 256 + numbers[1]; // Calculate result in decimal
+        if(result > 40 && result < 4096) { // Filter outliers
+            counts[result]++;
+            counter++;
         }
     }
     if(numbers[3] === 77 && numbers[4] === 67 && numbers[0] === 65) { // Check if first three codes are MCA
-        result = numbers[1] * 256 + numbers[2]; // Calculate result in decimal
-        if(result > 20 && result < 4100) { // Filter outliers
-            validatedResult = result;
+        const result = numbers[1] * 256 + numbers[2]; // Calculate result in decimal
+        if(result > 40 && result < 4096) { // Filter outliers
+            counts[result]++;
+            counter++;
         }
     }
     if(numbers[4] === 77 && numbers[0] === 67 && numbers[1] === 65) { // Check if first three codes are MCA
-        result = numbers[2] * 256 + numbers[3]; // Calculate result in decimal
-        if(result > 20 && result < 4100) { // Filter outliers
-            validatedResult = result;
+        const result = numbers[2] * 256 + numbers[3]; // Calculate result in decimal
+        if(result > 40 && result < 4096) { // Filter outliers
+            counts[result]++;
+            counter++;
         }
     }
 
-    if (connections.length > 0 && validatedResult !== 0) {
-        broadcast(JSON.stringify(validatedResult));
+    if(counter % 1000 === 0) {
+        broadcast(JSON.stringify(counts));
+        console.log('Broadcasted result.');
     }
 
-    validatedResult = 0;
-
-    console.log(counter++);
-
+    console.log(counter);
 }
